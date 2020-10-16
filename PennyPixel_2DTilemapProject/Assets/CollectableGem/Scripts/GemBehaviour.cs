16421971472df6b61116b054afc20176ce5c4bd7
minichibis <minichibis@gmail.com>
2020-10-15 19:05:47 -0500

    2d shit done

---
+++ PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -0,0 +1,46 @@
+﻿/** Sam Carpenter
+* these 2 things
+* im just not bringing the same fire anymore im sorry
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehaviour : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public GameObject collectedParticleSystem;
+	public CircleCollider2D gemCollider2D;
+
+	private float durationOfCollectedParticleSystem;
+
+
+	void Start()
+	{
+		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+	}
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player")) {
+			theCollider.GetComponent<WhyMustWeDoThis>().score++;
+			GemCollected ();
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gemVisuals.SetActive (false);
+		collectedParticleSystem.SetActive (true);
+		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+
+	}
+
+	void DeactivateGemGameObject()
+	{
+		gameObject.SetActive (false);
+	}
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Fox/WhyMustWeDoThis.cs
@@ -0,0 +1,39 @@
+﻿/** Sam Carpenter
+* these 2 things
+* im just not bringing the same fire anymore im sorry
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WhyMustWeDoThis : MonoBehaviour
+{
+	public Text scoretxt;
+	public Text wintext;
+	public int score;
+	public bool winned;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+		wintext.enabled = false;
+		winned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretxt.text = score.ToString();
+    }
+	
+	void OnTriggerEnter2D(Collider2D c){
+		if (c.CompareTag("Finish")) {
+			winned = true;
+			wintext.enabled = true;
+		}
+	}
+}
